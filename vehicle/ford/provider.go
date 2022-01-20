bd2fc02e644367bfc5aad41bc761678bd43c089e
andig <cpuidle@gmx.de>
2022-01-20 14:58:00 +0100

    simplify

--- vehicle/ford/provider.go
+++ vehicle/ford/provider.go
@@ -118,11 +118,7 @@ var _ api.VehicleOdometer = (*Provider)(nil)
 // Odometer implements the api.VehicleOdometer interface
 func (v *Provider) Odometer() (float64, error) {
 	res, err := v.statusG()
-	if res, ok := res.(StatusResponse); err == nil && ok {
-		return res.VehicleStatus.Odometer.Value, nil
-	}
-
-	return 0, err
+	return res.VehicleStatus.Odometer.Value, err
 }
 
 var _ api.VehiclePosition = (*Provider)(nil)
@@ -130,9 +126,5 @@ var _ api.VehiclePosition = (*Provider)(nil)
 // Position implements the api.VehiclePosition interface
 func (v *Provider) Position() (float64, float64, error) {
 	res, err := v.statusG()
-	if res, ok := res.(StatusResponse); err == nil && ok {
-		return res.VehicleStatus.Gps.Latitude, res.VehicleStatus.Gps.Longitude, nil
-	}
-
-	return 0, 0, err
+	return res.VehicleStatus.Gps.Latitude, res.VehicleStatus.Gps.Longitude, err
 }
